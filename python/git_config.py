465ac0662fb321e81bc6bb92d4bdbb1e673a4bdf
lijun <lijun@weibangong.com>
2017-05-24 16:36:10 +0800

    add git config file

---
+++ python/git_config.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import subprocess
+
+name = 'Ivan'
+email = 'lijundut@foxmail.com'
+
+def main():
+    subprocess.call(['git', 'user.name', name])
+    subprocess.call(['git', 'user.email' email])
+
+if __name__ = '__main__':
+    main()
